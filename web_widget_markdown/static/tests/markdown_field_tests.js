b8d07c4c0052bdf770fc7030a0d789d47b5c9d0d
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2022-11-23 07:19:49 +0400

    fix eslint

--- web_widget_markdown/static/tests/markdown_field_tests.js
+++ web_widget_markdown/static/tests/markdown_field_tests.js
@@ -1,22 +1,9 @@
 /** @odoo-module **/
+import { findChildren, getFixture } from "@web/../tests/helpers/utils";
+import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
 
-import {registry} from "@web/core/registry";
-import {makeFakeLocalizationService} from "@web/../tests/helpers/mock_services";
-import {
-    click,
-    clickCreate,
-    clickSave,
-    editInput,
-    getFixture,
-    triggerEvent,
-    findChildren,
-} from "@web/../tests/helpers/utils";
-import {makeView, setupViewRegistries} from "@web/../tests/views/helpers";
-
-const serviceRegistry = registry.category("services");
-
-let serverData;
-let target;
+let serverData = null;
+let target = null;
 
 const initMarkdownValue = `
 # Hello world
@@ -38,27 +25,11 @@ QUnit.module("web_widget_markdown", (hooks) => {
                             searchable: true,
                             trim: true,
                         },
-                        // bar: {string: "Bar", type: "boolean", default: true, searchable: true},
-                        // txt: {
-                        //     string: "txt",
-                        //     type: "text",
-                        // },
-                        // int_field: {
-                        //     string: "int_field",
-                        //     type: "integer",
-                        //     sortable: true,
-                        //     searchable: true,
-                        // },
-                        // qux: {string: "Qux", type: "float", digits: [16, 1], searchable: true},
                     },
                     records: [
                         {
                             id: 1,
-                            // bar: true,
                             content: initMarkdownValue,
-                            // int_field: 10,
-                            // qux: 0.44444,
-                            // txt: "some text",
                         },
                     ],
                 },
@@ -104,13 +75,13 @@ QUnit.module("web_widget_markdown", (hooks) => {
             resId: 1,
             serverData,
             arch: '<form><field name="content" widget="markdown"/></form>',
-            mockRPC: function (route, {args, method, model}) {
+            mockRPC: function (route, { args, method, model }) {
                 if (model === "partner" && method === "write") {
                     assert.strictEqual(args[1].content, "**bold content**", "should write correct value");
                 }
             },
         });
-        const markdownField = findChildren(concreteView, (comp) => comp.name == "MarkdownField");
+        const markdownField = findChildren(concreteView, (comp) => comp.name === "MarkdownField");
         const textarea = target.querySelector(".o_field_markdown textarea");
         assert.ok(textarea, "should have a text area");
         assert.strictEqual(textarea.value, initMarkdownValue, "should still be '# Hello world' in edit");
